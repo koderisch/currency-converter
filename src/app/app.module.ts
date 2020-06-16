1b2ec5069696fc45dc435cc3164ff794f05c12c0
Michael Koderisch <code@koderisch.co.uk>
2020-06-16 11:37:04 +0100

    removed some unnecessary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,8 +22,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
 
 
 @NgModule({
